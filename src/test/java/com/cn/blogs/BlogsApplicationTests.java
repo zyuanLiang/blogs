5478b91ddc349808f58cb5ba10261f63d3550b43
zhangyuan <18236773194@163.com>
2022-12-09 14:40:04 +0800

    init

---
+++ src/test/java/com/cn/blogs/BlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.blogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
